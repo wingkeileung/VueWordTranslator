03db2100064e86c2069257af302baa1d1ef7b566
Wing Kei Leung <winky.leung@gmail.com>
2017-11-21 19:38:51 -0800

    boiler plate for Vue JS

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Word Translator</h1>
+    <h5>Powered by Vue.js</h5>
+    <TranslateForm></TranslateForm>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+
+export default {
+  name: 'app',
+  components:{
+    TranslateForm
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="translateForm">
+    TranslateForm
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+}
+</script>
+
+<style>
+
+</style>
